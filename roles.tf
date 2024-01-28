823c501499e5d154c5f119bf284ed5cfed240e5e
Jimmy Casse <jcasse@macbook-pro-de-jimmy.home>
2024-01-28 17:23:06 +0000

    fix: tf fmt and adding roles creation

--- roles.tf
+++ roles.tf
@@ -1,14 +1,26 @@
 # A default role to map default policy for everyone
 resource "vault_jwt_auth_backend_role" "default" {
-  backend        = vault_jwt_auth_backend.oidc.path 
+  backend        = vault_jwt_auth_backend.oidc.path
   role_name      = var.oidc_default_role
-  token_policies = ["default"] 
- 
+  token_policies = ["default"]
+
   user_claim            = var.user_claim
   groups_claim          = var.groups_claim
   allowed_redirect_uris = var.allowed_redirect_uris
-  oidc_scopes           = ["https://graph.microsoft.com/.default"] 
+  oidc_scopes           = ["https://graph.microsoft.com/.default"]
   role_type             = "oidc"
 }
 
 # All others roles are defined with below resource declaration
+resource "vault_jwt_auth_backend_role" "roles" {
+  for_each       = var.teams
+  backend        = vault_jwt_auth_backend.oidc.path
+  role_name      = each.key
+  token_policies = [each.key]
+
+  user_claim            = var.user_claim
+  groups_claim          = var.groups_claim
+  allowed_redirect_uris = var.allowed_redirect_uris
+  oidc_scopes           = ["https://graph.microsoft.com/.default"]
+  role_type             = "oidc"
+}
--- variables.tf
+++ variables.tf
@@ -1,49 +1,54 @@
 variable "oidc_backend_description" {
-    description = "OIDC backend description"
-    default = "Main oidc authentication backend"
-    type = string
+  description = "OIDC backend description"
+  default     = "Main oidc authentication backend"
+  type        = string
 }
 
 variable "oidc_backend_path" {
-    description = "OIDC backend mount path"
-    default = "oidc"
-    type = string
+  description = "OIDC backend mount path"
+  default     = "oidc"
+  type        = string
 }
 
 variable "oidc_default_role" {
-    description = "Default oidc role"
-    default = "oidc-default" # using an oidc default role to map default pol
-    type = string
+  description = "Default oidc role"
+  default     = "oidc-default" # using an oidc default role to map default pol
+  type        = string
 }
 
 variable "oidc_client_id" {
-    description = "Azure OIDC client ID"
-    type = string
+  description = "Azure OIDC client ID"
+  type        = string
 }
 
 variable "oidc_client_secret" {
-    description = "Azure OIDC client secret"
-    type = string
+  description = "Azure OIDC client secret"
+  type        = string
 }
 
 variable "oidc_discovery_url" {
-    description = "Azure OIDC discovery url"
-    type = string
+  description = "Azure OIDC discovery url"
+  type        = string
 }
 
 variable "user_claim" {
   description = "User claim to use for user mapping"
-  type = string
-  default = "sub"
+  type        = string
+  default     = "sub"
 }
 
 variable "groups_claim" {
   description = "Groups claims to use for group mapping"
-  type = string
-  default = "groups"
+  type        = string
+  default     = "groups"
 }
 
 variable "allowed_redirect_uris" {
- description = "OIDC allowed redirect uris"
- type = list
+  description = "OIDC allowed redirect uris"
+  type        = list(any)
+}
+
+variable "teams" {
+  description = "A set containing all teams to create namespaces, policies and roles definition"
+  type        = map(any)
 }
