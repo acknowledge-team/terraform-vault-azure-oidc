b5cdaa74d3fdcfc9ccd09dfca838c62119171391
Jimmy Casse <jcasse@macbook-pro-de-jimmy.home>
2024-01-28 17:35:01 +0000

    feat: adding group mapping for oidc roles

---
+++ groups.tf
@@ -0,0 +1,13 @@
+resource "vault_identity_group" "group" {
+  for_each = var.teams
+  name     = each.key 
+  type     = "external"
+  policies = [each.key]
+}
+ 
+resource "vault_identity_group_alias" "group-alias" {
+    for_each = var.teams
+  name           = each.value // Azure group object ID 
+  mount_accessor = vault_jwt_auth_backend.oidc.accessor
+  canonical_id   = vault_identity_group.group[each.key].id
+}
